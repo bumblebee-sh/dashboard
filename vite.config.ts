d4a4582232276b313ada7ec235739f9781a993e9
Maksym Shmelyov <mailbox9111@gmail.com>
2025-02-07 09:35:38 +0100

    added git pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://bumblebee-sh.github.io/dashboard/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
